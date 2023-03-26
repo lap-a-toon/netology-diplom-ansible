e68dfdf957f280fb28e2fabb427b057a01ee7679
lap-a-toon <lap-a-toon@yandex.ru>
2023-03-26 19:27:10 +0500

    q

---
+++ install_node-exporter.sh
@@ -0,0 +1,6 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.0/node_exporter-1.0.0.linux-amd64.tar.gz 
+tar xvfz node_exporter-*.*-amd64.tar.gz
+cd node_exporter-*.*-amd64
+mkdir /etc/prometheus/node-exporter
+cp -r ./* /etc/prometheus/node-exporter
+chown -R prometheus:prometheus /etc/prometheus/node-exporter/
